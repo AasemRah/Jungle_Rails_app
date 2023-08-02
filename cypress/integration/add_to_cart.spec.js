b1ef72783d57def41280def4720876f4acd66674
Aasem Rahuma <aasem.rahuma@gmail.com>
2023-08-02 15:44:46 -0400

    adding products to cart

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -11,12 +11,16 @@ describe('add_to_cart functionality', () => {
     });
   });
 
-it('lets you add a product to the cart and checks that cart count increases by 1', () => {
-  cy.contains('My Cart (0)')
+  it('can visit the homepage', () => {
+    cy.visit('/')
+  })
 
-  cy.contains('Add').first().click({force: true})
+  it('lets you add a product to the cart and checks that cart count increases by 1', () => {
+    cy.contains('My Cart (0)')
 
-  cy.contains('My Cart (1)')
-})
+    cy.contains('Add').first().click({force: true})
+
+    cy.contains('My Cart (1)')
+  })
 
 });
