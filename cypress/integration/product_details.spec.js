ceb68aa186ff6a5045637c3ab257a0ad0b338d45
Aasem Rahuma <aasem.rahuma@gmail.com>
2023-08-02 15:22:59 -0400

    implementing a test for product details

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,33 @@
+describe('home page', () => {
+  it('can visit the homepage', () => {
+    const username = 'Jungle';
+    const password = 'Book';
+
+    cy.visit('http://localhost:3000', {
+      auth: {
+        username: username,
+        password: password,
+      },
+    });
+  });
+
+
+
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+     cy.get(".products article").should("have.length", 12);
+   });
+
+   it('lets you visit the selected product detail page', () => {
+    cy.visit('/')
+    cy.get(".products article").first().click()
+    cy.url().should("include", "/products/2")
+
+  })
+});
+
+
